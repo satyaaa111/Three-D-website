17d8a5fa7e19dbacc67110715f666960f521fadd
satyaaa111 <juman60000@gmail.com>
2024-01-14 12:11:12 +0530

    Added Sound Section and Display Section

--- src/components/Jumbotron.jsx
+++ src/components/Jumbotron.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import Iphone from '../assets/images/iphone-14.jpg'
+import HoldingIphone from '../assets/images/iphone-hand.png'
+
 
 
 function Jumbotron() {
+    const handleLearnMore = () =>{
+        const element = document.querySelector(".sound-section");
+        window.scrollTo({
+            top: element.getBoundingClientRect().top,
+            left: 0 , 
+            behavior: "smooth"
+        })
+    }
   return (
     <div className='jumbotron-section wrapper'>
       <h2 className='title'>New</h2>
@@ -16,9 +26,10 @@ function Jumbotron() {
             <button className='button'>Buy</button>
         </li>
         <li>
-            <a className='link'>Learn more</a>
+            <a className='link' onClick={handleLearnMore}>Learn more</a>
         </li>
       </ul>
+      <img className='iphone-img' src={HoldingIphone} alt='iPhone' />
     </div>
   )
 }
